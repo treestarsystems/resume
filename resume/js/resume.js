d2d41259cbe1c3b7e886d46d50793bc66c7d5cca
Tree Star Systems <info@treestarsystems.com>
2021-06-25 12:30:30 -0700

    06/25/21

--- resume/js/resume.js
+++ resume/js/resume.js
@@ -3,13 +3,212 @@ var resumeConfig = {
  "resumeTitle":"Full Stack Developer",
  "resumeBirthDate":"11/20/1985",
  "resumeSiteLink":"https://treestarsystems.com",
+ "resumeGithubUserName":"treestarsystems",
  "resumeGithubLink":"https://github.com/treestarsystems",
  "resumeYoutubeLink":"https://www.youtube.com/watch?v=oYOdsJXKPFY&list=PLFwrukKhzwLjHuygfSlw97LGS0g5_pzGM",
  "resumeUpworkLink":"https://www.upwork.com/o/profiles/users/~0137ac5fc8e430492d/",
- "":"",
+ "resumeResidence":"Waycross, GA",
+ "resumeMap":`
+  <iframe frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src=
+   "https://maps.google.com/maps?q=Waycross%20Georgia%2C%20USA&amp;amp;t=m&amp;amp;z=16&amp;amp;output=embed&amp;amp;iwloc=near&amp;output=embed"
+  ></iframe>
+ `,
+ "resumeEmail":"info@treestarsystems.com",
+ "resumePhone":"954-240-1167",
+ "resumeAboutMe":`Well rounded Network and Systems Administrator with experience in LAN/WAN/WLAN data and
+telecommunications, security practices and technologies, as well as troubleshooting
+approaches. I am a heavy user of open source alternatives to supplement proprietary/closed
+source environments to ensure uptime, security, and overall health while achieving a lower Total
+Cost of Ownership (TCO). My goal is to never work too hard doing a repetitive task but work
+hard to automate a task. I write code to make my job easier, streamline my department’s
+process, and make my colleagues' lives better by allowing them to work on tasks that really
+matter to them. My ultimate goal is to propel and accelerate the growth of my employer.`,
+ "resumeCompetancies":[
+  {
+   "logo": "fab fa-node competency-logo",
+   "imageAltText":"node js logo",
+   "text":"I love to code backend functionality. Ingesting, normalizing, and storing data is fun to me. I crave large data sets and work to find ways to process information as fast and reliably as possible.",
+   "competencyName":"Node.js",
+   "competencyFontAwesomeClassName":"fab fa-node-js",
+  },
+  {
+   "logo": "fab fa-js-square competency-logo",
+   "imageAltText":"vanilla js logo",
+   "text":"I work to make it user friendly, viusally pleasing, and highly functional. I have learned the hard way that if the front end is not working well for a user base the backend means absolutely nothing. ",
+   "competencyName":"vanilla JS",
+   "competencyFontAwesomeClassName":"fab fa-js",
+  },
+  {
+   "logo": "fab fa-html5 competency-logo",
+   "imageAltText":"HTML5 logo",
+   "text":"There is nothing like a well placed anchor tag or class that allows you to hydrate your page with dynamic content. HTML5 with a templating engine or framework opens up a ton of possibilities that will bring life to your page.",
+   "competencyName":"HTML5",
+   "competencyFontAwesomeClassName":"fab fa-html5",
+  },
+  {
+   "logo": "fab fa-css3-alt competency-logo",
+   "imageAltText":"CSS logo",
+   "text":"I have a love hate relationship with CSS. I love it when someone else does it for me, but hate it when I have to make it work. With the use of CSS Variables I can do all kinds of fun stuff. I mean how else do you think I make the colors change every time you reload this page? <i style='font-style:normal;'>&#129300;&#129299;</i>",
+   "competencyName":"CSS3",
+   "competencyFontAwesomeClassName":"fab fa-css3",
+  },
+ ],
+ "resumePricingState":"off",
+ "resumePricing":[
+  {
+   "pricingLevel":"Basic",
+   "pricingRate":"35",
+   "pricingList":[
+    "Item 1",
+    "Item 2",
+    "",
+    "",
+   ],
+  },
+  {
+   "pricingLevel":"Intermediate",
+   "pricingRate":"55",
+   "pricingList":[
+    "Includes Basic",
+    "",
+    "",
+    "",
+   ],
+  },
+  {
+   "pricingLevel":"Advance",
+   "pricingRate":"75",
+   "pricingList":[
+    "Includes Intermediate",
+    "",
+    "",
+    "",
+   ],
+  },
+ ],
+ "resumeEducation":[
+  {
+   "range":"2004-2006",
+   "gpa":"GPA 2.8",
+   "schoolName":"Missouri Valley College",
+   "fieldOfStudy":"Computer Science",
+   "description":"",
+  },
+  {
+   "range":"2006-2008",
+   "gpa":"GPA 2.8",
+   "schoolName":"Broward College",
+   "fieldOfStudy":"Associates of the Arts",
+   "description":"",
+  },
+ ],
+ "resumeExperience":[
+  {
+   "range":"Dec 2019-Present",
+   "companyName":"Hamilton Home Loans",
+   "title":"Tier 3 Support/Full Stack Developer",
+   "description":"",
+  },
+  {
+   "range":"2009-2019",
+   "companyName":"MJ Network Solutions",
+   "title":"Lead Technician/Senior Consultant",
+   "description":"",
+  },
+  {
+   "range":"Aug 2011–2020",
+   "companyName":"365 Datacenters",
+   "title":"Network Operations (NetOps)",
+   "description":"",
+  },
+  {
+   "range":"2008–2010",
+   "companyName":"DeRose Technologies Inc.",
+   "title":"Technician/Consultant",
+   "description":"",
+  },
+  {
+   "range":"2007-2008",
+   "companyName":"Reliant Medical",
+   "title":"Systems and Network Administrator",
+   "description":"",
+  },
+ ],
+ "resumeSkills":[
+  {
+   "skillTitle":"Coding",
+   "profeciencies":[
+    {
+     "name":"API Design",
+     "percentage":"75",
+    },
+    {
+     "name":"UI/UX Design",
+     "percentage":"50",
+    }
+   ]
+  },
+  {
+   "skillTitle":"Coding2",
+   "profeciencies":[
+    {
+     "name":"API Design",
+     "percentage":"75",
+    },
+    {
+     "name":"UI/UX Design",
+     "percentage":"50",
+    }
+   ]
+  }
+ ],
+ "resumeKnowledges": [
+  "Tai Lopez",
+  "NAH-LEDGE",
+  "stuf",
+  "the man",
+ ],
+ "resumeCertificates":[
+  {
+   "title":"CCNA (200-125)",
+   "vendor":"Cisco",
+   "id":"CSCO11806465",
+   "logo":"cisco.png",
+   "date":"Dec. 2019"
+  },
+  {
+   "title":"CompTIA A+",
+   "vendor":"CompTIA",
+   "id":"5FBBJY92EHQES5GF",
+   "logo":"comptia.png",
+   "date":""
+  },
+  {
+   "title":"CompTIA Network+",
+   "vendor":"CompTIA",
+   "id":"C07ZG2JJYKR1SST3",
+   "logo":"comptia.png",
+   "date":""
+  },
+  {
+   "title":"CompTIA Linux+",
+   "vendor":"CompTIA",
+   "id":"JK4DV87L63FESTV7",
+   "logo":"comptia.png",
+   "date":""
+  },
+  {
+   "title":"IPv6 Certified: Sage Level",
+   "vendor":"Hurricane Electric",
+   "id":"(Certificate Available)",
+   "logo":"hurricane-electric.png",
+   "date":""
+  },
+ ],
+ "resume":"",
 }
 
-axios.get(`https://api.github.com/users/treestarsystems/repos`, {})
+axios.get(`https://api.github.com/users/${resumeConfig.resumeGithubUserName}/repos`, {})
  .then((response) => {
   let targetDiv = document.getElementById('repoList');
   let rawRepoList = response.data;
@@ -34,7 +233,7 @@ axios.get(`https://api.github.com/users/treestarsystems/repos`, {})
        <div class="post-date">Created: ${createdDate.toDateString()}</div>
        <div class="post-date">| Updated: ${updatedDate.toDateString()}</div>
        <a href="${rawRepoList[i].html_url}" target="_blank">
-         <h3 class="blog-item-title">${rawRepoList[i].name}</h3>
+         <h3 class="blog-item-title repo-list-description">${rawRepoList[i].name}</h3>
          <h5 class="blog-item-title repo-list-description">${rawRepoList[i].description}</h5>
        </a>
       </div>
@@ -49,34 +248,171 @@ axios.get(`https://api.github.com/users/treestarsystems/repos`, {})
 //Source: https://www.javatpoint.com/calculate-age-using-javascript
 function calculateAge (birthDay) {
  var dob = new Date(birthDay);
- //calculate month difference from current date in time
  var month_diff = Date.now() - dob.getTime();
- //convert the calculated difference in date format
  var age_dt = new Date(month_diff);
- //extract year from date
  var year = age_dt.getUTCFullYear();
- //now calculate the age of the user
  var age = Math.abs(year - 1970);
  return age;
 }
 
 function fillResume (obj) {
  document.title = `Resume: ${obj.fullName} - ${obj.resumeTitle}`;
- let age = calculateAge(obj.resumeBirthDate);
+ let ageDiv = document.getElementById('age');
+ let competenciesDiv = document.getElementById('competencies');
+ let pricingDivState = document.getElementById('resumePricingState');
+ let knowledgesDiv = document.getElementById('resumeKnowledges');
+ let skillsDiv = document.getElementById('resumeSkills');
+ let certificatesDiv = document.getElementById('resumeCertificates');
+ let educationDiv = document.getElementById('resumeEducation');
+ let experienceDiv = document.getElementById('resumeExperience');
+
+ ageDiv.innerHTML = calculateAge(obj.resumeBirthDate);
  for (let key in obj) {
-  if (key == "fullName") {
-   let element = document.querySelectorAll(`.${key}`);
-   element.forEach((e) => {
-    e.innerHTML = obj[key];
-   });
-  }
-  if (key.includes('Link')) {
-   let element = document.querySelectorAll(`.${key}`);
-   element.forEach((e) => {
+  let element = document.querySelectorAll(`.${key}`);
+  element.forEach((e) => {
+   if (key.includes('Link')) {
     e.href = obj[key];
-   });
-  }
+   } else {
+    e.innerHTML = obj[key].replace(/[\n\r]/g, ' ');
+   }
+  });
  }
+ obj.resumeCompetancies.forEach((e,i) => {
+  competenciesDiv.innerHTML += `
+   <div class="testimonial">
+    <i class="${e.logo}"></i>
+    <div class="text">
+     <p>${e.text}</p>
+    </div>
+    <div class="author-info">
+     <h4 class="author">${e.competencyName}</h4>
+     <div class="icon">
+      <i class="${e.competencyFontAwesomeClassName}"></i>
+     </div>
+    </div>
+   </div>
+  `;
+ });
+ //Conditional based off config object property resumePricingState. Which can be on/off.
+ if (obj.resumePricingState == "on") {
+  pricingDivState.style.display = "block";
+  let pricingDiv = document.getElementById('pricing');
+  obj.resumePricing.forEach((e,i) => {
+   function generateList (listItemsArray) {
+    let html = '';
+    for (let i = 0; i < listItemsArray.length; i++) {
+     html += `<div class="fw-default-row">${listItemsArray[i]}</div>`
+    }
+    return html;
+   }
+   pricingDiv.innerHTML += `
+    <div class="fw-package-wrap col-md">
+     <div class="fw-package">
+      <div class="fw-heading-row">
+       <span>${e.pricingLevel}</span>
+      </div>
+      <div class="fw-pricing-row">
+       <span>$${e.pricingRate}</span>
+       <small>per hour</small>
+      </div>
+      ${generateList(e.pricingList)}
+     </div>
+    </div>
+   `;
+  });
+ }
+ //Fill in Knowledges Area
+ knowledgesDiv.innerHTML = "";
+ obj.resumeKnowledges.forEach((e,i) => {
+  knowledgesDiv.innerHTML += `<li>${e}</li>`;
+ });
+ //Fill in Skills Area
+ skillsDiv.innerHTML = "";
+ obj.resumeSkills.forEach((e,i) => {
+  if (e.skillTitle) {
+   skillsDiv.innerHTML += `
+    <div class="block-title"><h3> ${e.skillTitle} <span>Skills</span></h3></div>
+    <div class="skills-info skills-second-style" id="${e.skillTitle.toLowerCase()}-${i}"></div>
+    <div class="white-space-10"></div>
+   `;
+   let innerSkillsDiv = document.getElementById(`${e.skillTitle.toLowerCase()}-${i}`);
+   for (let pi = 0; pi < e.profeciencies.length; pi++) {
+    innerSkillsDiv.innerHTML += `
+     <div class="skill clearfix">
+      <h4>${e.profeciencies[pi].name}</h4>
+      <div class="skill-value">${e.profeciencies[pi].percentage}%</div>
+     </div>
+     <div class="skill-container skill-${pi}">
+      <div class="skill-percentage" style="width:${e.profeciencies[pi].percentage}%"></div>
+     </div>
+    `;
+    if (e.profeciencies.length-1 == pi) innerSkillsDiv.innerHTML += `</div><div class="white-space-10"></div>`;
+   }
+  }
+ });
+ //Fill in Certificates Area
+ certificatesDiv.innerHTML = "";
+ obj.resumeCertificates.forEach((e,i) => {
+  certificatesDiv.innerHTML += `
+   <div class="col-xs-12 col-sm-6">
+    <div class="certificate-item clearfix">
+     <div class="certi-logo">
+      <img src="images/${e.logo}" alt="${e.vendor}-logo" onerror="this.parentElement.innerHTML='${e.vendor}'">
+     </div>
+     <div class="certi-content">
+      <div class="certi-title">
+       <h4>${e.title}</h4>
+       <h6 style="font-weight: 400;font-style: italic;">${e.vendor}</h6>
+      </div>
+      <div class="certi-id">
+       <span>ID: ${e.id}</span>
+      </div>
+      <div class="certi-date">
+       <span>${e.date}</span>
+      </div>
+      <div class="certi-company">
+       <span></span>
+      </div>
+     </div>
+    </div>
+   </div>
+  `;
+ });
+ //Fill in Education Area
+ educationDiv.innerHTML = "";
+ obj.resumeEducation.forEach((e,i) => {
+  educationDiv.innerHTML += `
+   <div class="timeline-item clearfix">
+    <div class="left-part">
+     <h5 class="item-period">${e.range}</h5>
+     <span class="item-company" style="font-weight: 600;">${e.gpa}</span>
+     <span class="item-company">${e.schoolName}</span>
+    </div>
+    <div class="divider"></div>
+    <div class="right-part">
+     <h4 class="item-title">${e.fieldOfStudy}</h4>
+     <p>${e.description}</p>
+    </div>
+   </div>
+  `;
+ });
+ //Fill in Experience Area
+ experienceDiv.innerHTML = "";
+ obj.resumeExperience.forEach((e,i) => {
+  experienceDiv.innerHTML += `
+   <div class="timeline-item clearfix">
+    <div class="left-part">
+     <h5 class="item-period">${e.range}</h5>
+     <span class="item-company">${e.companyName}</span>
+    </div>
+    <div class="divider"></div>
+    <div class="right-part">
+     <h4 class="item-title">${e.title}</h4>
+     <p>${e.description}</p>
+    </div>
+   </div>
+  `;
+ });
 }
 
 //Take an array of image file names and the id of the img element
@@ -91,84 +427,11 @@ function randomPicture(pa) {
 
 function randomPageColor(ca) {
  let selectedObj = ca[Math.floor(Math.random() * ca.length)];
- let pageContentClass = document.querySelector('.page-content');
- let pageTitleClass = document.querySelectorAll('.page-title');
- let pageTitleAfterClass = document.querySelectorAll('.page-title:after');
- let activeLinkClass = document.querySelector('ul.main-menu a.active');
- let menuIconClass = document.querySelectorAll('.menu-icon');
- let titleClass = document.querySelectorAll('.title');
- let ciIconClass = document.querySelectorAll('.ci-icon > .lnr');
- let lmInfoBlockClass = document.querySelectorAll('.lm-info-block > .lnr');
- let knowledgesClass = document.querySelectorAll('.knowledges > li');
- let skillContainerClass = document.querySelectorAll('.skill-container');
- let skillPercentageClass = document.querySelectorAll('.skill-percentage');
- let linkTextClass = document.querySelectorAll('.link-text');
- pageContentClass.style.background = `${selectedObj.backgroundBasic}`;
-
- pageTitleClass.forEach((e,i) => {
-  e.querySelector('h2').style.color = `${selectedObj.hoverColor}`;
-//  e.style.setProperty(':after',`background-image: -webkit-repeating-radial-gradient(center center, ${selectedObj.hoverColor}, ${selectedObj.hoverColor} 1px, transparent 0px, transparent 100%);`)
-/*
-  let pseudoElement = window.getComputedStyle(e,':after');
-  console.log(pseudoElement);
-  pseudoElement.color = `${selectedObj.backgroundBasic}`;
-*/
- });
-
-// activeLinkClass.style.color = `${selectedObj.hoverColor}`;
-
- titleClass.forEach((e,i) => {
-  e.style.color = `${selectedObj.hoverColor}`;
- });
-
- ciIconClass.forEach((e,i) => {
-  e.style.color = `${selectedObj.hoverColor}`;
- });
-
- lmInfoBlockClass.forEach((e,i) => {
-  e.style.color = `${selectedObj.hoverColor}`;
- });
-
- knowledgesClass.forEach((e,i) => {
-  e.style.backgroundColor = `${selectedObj.hoverColor}`;
- });
-
- skillContainerClass.forEach((e,i) => {
-  e.style.border = `1px solid ${selectedObj.hoverColor}`;
- });
-
- skillPercentageClass.forEach((e,i) => {
-  e.style.backgroundColor = `${selectedObj.hoverColor}`;
- });
-
- linkTextClass.forEach((e,i) => {
-  e.style.backgroundColor = `${selectedObj.hoverColor}`;
- });
-
- menuIconClass.forEach((e,i) => {
-  e.onmouseover = () => {
-   e.style.color = `${selectedObj.hoverColor}`;
-  };
-  e.onmouseout = () => {
-   e.style.color = `#b5b6b7`;
-  };
- });
-}
-
-function modifyStyleSheet () {
- let styleSheets = document.styleSheets;
- for (let i = 0; i < document.styleSheets.length; i++) {
-  let targetCSS = document.styleSheets[i].href;
-  if (targetCSS.includes('main.css')) {
-   let rules = document.styleSheets[i].rules;
-   for (let ir = 0; ir < rules.length; ir++) {
-    if (rules[ir].cssText.includes('80, 80, 245')) {
-//     console.log(rules[ir].cssText);
-     console.log(rules[ir]);
-    }
-   }
-  }
- }
+ document.documentElement.setAttribute("style", `
+  --resume-color-primary-1: ${selectedObj.resumeColorPrimaryOne};
+  --resume-color-primary-2: ${selectedObj.resumeColorPrimaryTwo};
+  --resume-color-secondary: ${selectedObj.resumeColorSecondary}
+ `);
 }
 
 var pictureArray = [
@@ -204,13 +467,18 @@ var pictureArray = [
 ];
 
 var colorArray = [
- {"backgroundBasic":"linear-gradient(#fe6d72, #fea621)","backgroundWebkit":"-webkit-gradient(linear, left top, left bottom, from(#fe6d72), to(#fea621))","hoverColor":"#fea621"},
- {"backgroundBasic":"linear-gradient(#21e1ae, #09aeea)","backgroundWebkit":"-webkit-gradient(linear, left top, left bottom, from(#21e1ae), to(#09aeea))","hoverColor":"#09aeea"},
- {"backgroundBasic":"linear-gradient(#0b7080, #0ba376)","backgroundWebkit":"-webkit-gradient(linear, left top, left bottom, from(#0b7080), to(#0ba376))","hoverColor":"#0ba376"},
- {"backgroundBasic":"linear-gradient(#9d2bf3, #5050f5)","backgroundWebkit":"-webkit-gradient(linear, left top, left bottom, from(#9d2bf3), to(#5050f5))","hoverColor":"#5e2fd6","hoverColor":"#5e2fd6"}
+ {"resumeColorPrimaryOne":"#5050f5","resumeColorPrimaryTwo":"#9d2bf3","resumeColorSecondary":"#5e2fd6"},
+ {"resumeColorPrimaryOne":"#0ba376","resumeColorPrimaryTwo":"#0b7080","resumeColorSecondary":"#0ba376"},
+ {"resumeColorPrimaryOne":"#09aeea","resumeColorPrimaryTwo":"#21e1ae","resumeColorSecondary":"#09aeea"},
+ {"resumeColorPrimaryOne":"#fea621","resumeColorPrimaryTwo":"#fe6d72","resumeColorSecondary":"#fea621"}
 ];
 
 randomPicture(pictureArray);
 randomPageColor(colorArray);
 fillResume(resumeConfig);
-modifyStyleSheet();
+document.addEventListener('readystatechange', event => {
+ if (event.target.readyState === 'complete') {
+  let targetOwlDiv = document.getElementById('competencies').querySelector('.owl-stage-outer.owl-height');
+  targetOwlDiv.style.height = 'auto';
+ }
+});
